f0682308db6000f79346d8dacbb653ddfc394a30
HarryWu00 <8hwu@stpatrick.org>
2017-02-01 11:26:16 -0600

    Day 1

---
+++ degree.js
@@ -0,0 +1,32 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+  let sum [] == 0;
+  let degreelist == A.size();
+  for(var d=0; d<list.length; d++){
+    return sum
+  }
+}
+
+function degreecentrality(){
+
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
+
+
+
+Write a for-loop which will go through that degreelist one by one and add it into the sum.
++  Return the sum
