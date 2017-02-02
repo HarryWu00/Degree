aea906ff45f7d5115d98bbd86cca7a7cc73cbbae
HarryWu00 <8hwu@stpatrick.org>
2017-02-02 10:49:03 -0600

    Day 2

--- degree.js
+++ degree.js
@@ -9,14 +9,17 @@ let A = Matrix([
 ]);
 //Code here
 function degree(mtrx,v){
-  let sum [] == 0;
+  let sum == 0;
   let degreelist == A.size();
-  for(var d=0; d<list.length; d++){
-    return sum
+  let degreelist = mtrx(v-1);
+  for(var a=0;a<degreelist.length;a++){
+    sum += (degreelist[a]);
   }
+  return sum;
 }
 
-function degreecentrality(){
+function degreecentrality(mtrx,v){
+  let neighbors
 
 }
 
@@ -25,8 +28,3 @@ function degreecentrality(){
 //Run test code with ctrl+shift+p  then type in "script"
 console.log(degree(A,3));
 console.log(degreecentrality(A,2));
-
-
-
-Write a for-loop which will go through that degreelist one by one and add it into the sum.
-+  Return the sum
